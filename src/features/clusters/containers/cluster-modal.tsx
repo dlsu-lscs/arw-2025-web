58baf1dc7f43e7027dafac01e071f48904372e53
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-09-05 00:36:39 +0800

    feat: added engage card and first grids

--- src/features/clusters/containers/cluster-modal.tsx
+++ src/features/clusters/containers/cluster-modal.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client'
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
-import { useClusterModalStore } from "../store/useClusterModalStore";
-import CloseModal from "@/components/modal/close-modal";
+} from '@/components/ui/dialog'
+import { useClusterModalStore } from '../store/useClusterModalStore'
+import CloseModal from '@/components/modal/close-modal'
 
 export default function ClusterModal() {
-  const { isOrgModalOpen, closeOrgModal } = useClusterModalStore();
+  const { isOrgModalOpen, closeOrgModal } = useClusterModalStore()
   return (
     <>
       <Dialog open={isOrgModalOpen} onOpenChange={closeOrgModal}>
@@ -19,21 +19,39 @@ export default function ClusterModal() {
               <CloseModal />
             </DialogTitle>
           </DialogHeader>
-          <div>
-            <h1 className="text-2xl">Clusters!?</h1>
-            <p className="font-space-mono">
-              Clusters are alliances or groups of CSO-accredited organizations
-              that share similar themes or goals. Select one that catches your
-              eye, and find where you belong!
-            </p>
-          </div>
-          <div className="flex justify-center">
+          <main className="grid grid-cols2 gap-4">
+            <section className="grid grid-cols-2 gap-2">
+              <section className="grid grid-cols-1 gap-4">
+                <div>
+                  <h1 className="text-2xl">Clusters!?</h1>
+                  <p className="font-space-mono">
+                    Clusters are alliances or groups of CSO-accredited
+                    organizations that share similar themes or goals. Select one
+                    that catches your eye, and find where you belong!
+                  </p>
+                </div>
+                <div>
+                  <div className="w-full h-[200px] bg-gray-300 flex justify-center items-center rounded-lg text-white">
+                    <div className="flex flex-col gap-2">
+                      <h1 className="text-left text-4xl [text-shadow:_4px_4px_0px_rgba(0,0,0,1)]">
+                        ENGAGE
+                      </h1>
+                      <p className="text-md">
+                        Engineering Alliance Geared Towards Excellence
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </section>
+          </main>
+          <footer className="flex justify-center">
             <h3 className="font-tiny5 opacity-50">
               Powered by La Salle Computer Society.
             </h3>
-          </div>
+          </footer>
         </DialogContent>
       </Dialog>
     </>
-  );
+  )
 }
