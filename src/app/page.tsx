8608f5766e769c3fcc7cb83df1a3245d2dc229ae
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-09-07 09:58:39 +0800

    feat: fetched and rendered orgs card using isr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HomePage from '@/features/home/container/home-page';
 import { requireAuth } from '@/features/auth/services/server-auth';
 import { redirect } from 'next/navigation';
 import { getAllOrgs } from '@/features/orgs/services/orgs.services';
+import { OrgsResponse } from '@/features/orgs/types/orgs.types';
 
 export default async function Home() {
   const user = await requireAuth();
@@ -9,11 +10,11 @@ export default async function Home() {
   if (!user) redirect('/auth/login');
   if (user.needsRefresh) redirect('/auth/refresh');
 
-  const orgs = await getAllOrgs('', 0, 5);
+  const initialOrgs: OrgsResponse = await getAllOrgs('', 0, 5);
   return (
     <>
       <div className="min-h-screen p-8">
-        <HomePage user={user} initialOrgs={orgs} />
+        <HomePage user={user} initialOrgs={initialOrgs} />
       </div>
     </>
   );
